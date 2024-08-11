3902d46f4ed0c2d2a0ed8c077a9ecf5cc32e9a30
RIKHITAKOGANTI <60394491+RIKHITAKOGANTI@users.noreply.github.com>
2024-08-10 19:01:24 -0500

    Update SpringBootRestApplication.java

--- backend/src/main/java/com/learn/springbootrest/SpringBootRestApplication.java
+++ backend/src/main/java/com/learn/springbootrest/SpringBootRestApplication.java
@@ -1,4 +1,4 @@
-package com.telusko.springbootrest;
+package com.learn.springbootrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
